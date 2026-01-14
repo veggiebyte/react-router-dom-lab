7ec4e64068b67ba4c002d4653e1238102878e6ed
“CindyS” <veggiebyte@yahoo.com>
2026-01-13 22:18:34 -0800

    Nav bar, used code from lecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 // src/App.jsx
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import NavBar from './components/NavBar/NavBar';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import MailboxList from './components/MailboxList/MailboxList';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox = (formData) => {
+    const newMailbox = {
+      _id: mailboxes.length + 1,
+      ...formData
+    };
+    setMailboxes([...mailboxes, newMailbox]);
+  };
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
